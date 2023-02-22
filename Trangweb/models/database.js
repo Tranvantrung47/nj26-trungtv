1b6e76fe61261a0150613ed9afac24b58407828b
Tranvantrung47 <trantrung4797@gmail.com>
2023-02-22 10:59:44 +0700

    update

---
+++ Trangweb/models/database.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "Phaptrung_Cakes",
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.log("Database is failed to connect!", error);
+    return;
+  }
+  console.log(
+    "Database is connected successfully \nServer is running on port localhost:3000"
+  );
+});
+
+module.exports = db;
